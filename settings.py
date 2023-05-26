4ab13f8a97bbfe97b5b4cb6504e27404bdecbca7
Jylia1994 <125107147+Jylia1994@users.noreply.github.com>
2023-05-27 00:13:19 +0800

    Update settings.py
    Валидные данные были добавлены в файл .env
    Если вдруг не сработает закомментила свои данные в этом файле

--- settings.py
+++ settings.py
@@ -5,3 +5,6 @@ load_dotenv()
 
 valid_email = os.getenv('valid_email')
 valid_password = os.getenv('valid_password')
+
+# valid_email = "volkdarina94@gmail.com"
+# valid_password = "12345darin@volk"
